b402d120d45278ddb5952c4a4e4669218d844a7d
Huiying Lin <hlin0023@student.monash.edu>
2025-12-03 10:38:00 +1100

    fix education courses

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,29 +7,30 @@ const Education = () => {
       <h2>Education</h2>
 
       <div className="education-item">
-        <h1>Master of Information Technology (Artificial Intelligence)</h1>
-        <a> </a>
+        <h3>Master of Information Technology (Artificial Intelligence)</h3>
         <p className="institution">The University of Melbourne • 2025 - current</p>
         <div className="coursework">
           <h4>Relevant Coursework:</h4>
+          <ul className="coursework-list">
             <li>Machine Learning</li>
-            <li>Computer Vison</li>
+            <li>Computer Vision</li>
+          </ul>
         </div>
       </div>
   
       <div className="education-item">
-        <h1>Bachelor of Computer Science (with Distinction)</h1>
-        <a> </a>
+        <h3>Bachelor of Computer Science (with Distinction)</h3>
         <p className="institution">Monash University • 2020 - 2024</p>
         <div className="coursework">
           <h4>Relevant Coursework:</h4>
+          <ul className="coursework-list">
             <li>Distributed Systems</li>
             <li>Software Engineering</li>
             <li>Databases</li>
             <li>Algorithms</li>
             <li>Cloud Computing</li>
             <li>Full Stack Development</li>
-
+          </ul>
         </div>
       </div>
     </section>
